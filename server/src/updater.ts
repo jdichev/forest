1866d46b350636d1a2bce1d4fa38e1b5716b8619
Jordan Dichev <jordan.dichev@gmail.com>
2022-05-05 21:57:46 +0300

    Update frequency decreased

--- server/src/updater.ts
+++ server/src/updater.ts
@@ -10,15 +10,13 @@ export default class Updater {
   private static task: cron.ScheduledTask;
 
   public static start() {
-    const UPDATE_FREQUENCY_MINUTES = 30;
-
     const feedUpdater = new FeedUpdater();
 
-    pino.debug(`Updating every ${UPDATE_FREQUENCY_MINUTES} minutes`);
+    pino.debug(`Updating regularly`);
     feedUpdater.updateItems();
 
     this.task = cron.schedule(
-      `*/${UPDATE_FREQUENCY_MINUTES} * * * *`,
+      `1 * * * *`,
       () => {
         feedUpdater.updateItems();
       },
@@ -29,7 +27,7 @@ export default class Updater {
 
     setTimeout(() => {
       this.task.start();
-    }, 1000 * 60);
+    }, 1000 * 60 * 2);
   }
 
   public static stop() {
