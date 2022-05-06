724f4b21f6a05a6be1842adfcb0fc013081ddc26
Jordan Dichev <jordan.dichev@gmail.com>
2022-05-07 00:38:18 +0300

    Upgraded server package dependencies

--- server/src/server.test.ts
+++ server/src/server.test.ts
@@ -2,19 +2,20 @@ import request from "supertest";
 import server from "./server";
 
 describe("GET /users", function () {
-  it("responds with json", async (done) => {
-    const app = await server.start();
-    return request(app)
-      .get("/users")
-      .expect("Content-Type", /json/)
-      .expect(404)
-      .then((response) => {
-        expect(response.body.message).toEqual("Not found");
+  it("responds with json", (done) => {
+    server.start().then((app) => {
+      return request(app)
+        .get("/users")
+        .expect("Content-Type", /json/)
+        .expect(404)
+        .then((response) => {
+          expect(response.body.message).toEqual("Not found");
 
-        server.stop();
+          server.stop();
 
-        return done();
-      })
-      .catch((err) => done(err));
+          return done();
+        })
+        .catch((err) => done(err));
+    });
   });
 });
