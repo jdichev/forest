e37d0217c09bd76bf5241fea67fc36ef7e67b4f5
Jordan Dichev <jordan.dichev@gmail.com>
2022-11-06 18:59:46 +0200

    Dependencies updated

--- server/src/modules/OpmlParser.ts
+++ server/src/modules/OpmlParser.ts
@@ -1,22 +1,24 @@
-// @ts-nocheck
-import parser, { tNode } from "txml";
+import * as parser from "txml";
 
 const opmlParser = {
   load(opmlString: string) {
-    const xlmContent: tNode[] = parser.parse(opmlString);
+    const xlmContent = parser.parse(opmlString);
 
     const startNode = xlmContent[1].children[1].children;
 
+    // @ts-ignore
     const categories = startNode.map((node) => {
       return {
         title: node.attributes.title,
         text: node.attributes.text,
       };
     });
-
+    
     const feeds: Feed[] = [];
-
+    
+    // @ts-ignore
     startNode.forEach((node) => {
+      // @ts-ignore
       node.children.forEach((feedEl) => {
         feeds.push({
           categoryTitle: node.attributes.title,
