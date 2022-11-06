a9aee883d41c440bd82a248e4ed61a12a82c07c9
Jordan Dichev <jordan.dichev@gmail.com>
2022-11-06 18:58:40 +0200

    Postinstall command sequence reworked

--- scripts/install.js
+++ scripts/install.js
@@ -1,6 +1,10 @@
 const { execSync } = require("child_process");
 
 const run = async (command, cwd) => {
+  console.log("-".repeat(80));
+  console.log(`Running "${command}" in "${cwd}"`);
+  console.log("-".repeat(80));
+
   execSync(command, {
     cwd: cwd,
     stdio: "inherit",
@@ -8,18 +12,17 @@ const run = async (command, cwd) => {
 };
 
 const install = async () => {
-  run("npm install", "./config");
   run("npm link", "./config");
+  run("npm link", "./webapp");
+  run("npm link", "./server");
+
+  run("npm install", "./config");
 
   run("npm link forestconfig", "./webapp");
   run("npm install", "./webapp");
-  run("npm link", "./webapp");
-  run("npm run build", "./webapp");
 
   run("npm link forestconfig", "./server");
   run("npm install", "./server");
-  run("npm link", "./server");
-  run("npm run build", "./server");
 
   run("npm link forestconfig forestserver forestwebapp", "./desktop");
   run("npm install", "./desktop");
