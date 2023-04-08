42f7eb0850749e35113aacf7e94b363b7c1b180c
Jordan Dichev <jordan.dichev@gmail.com>
2023-04-08 13:10:00 +0300

    Removed unused code

--- helpers/fetch-feed/test-fetch-sync.js
+++ helpers/fetch-feed/test-fetch-sync.js
@@ -3,12 +3,10 @@ const { fetchFeedSync } = require("./fetch-feed-sync");
 // Fetch atom feed
 const atomFeedResStr = fetchFeedSync("http://localhost:3000/test.atom");
 const atomFeedRes = JSON.parse(atomFeedResStr);
-// console.log(atomFeedRes);
 
 // Fetch rss feed
 const rssFeedResStr = fetchFeedSync("http://localhost:3000/test.rss");
 const rssFeedRes = JSON.parse(rssFeedResStr);
-// console.log(rssFeedRes);
 
 console.assert(
   atomFeedRes.items[0].title === rssFeedRes.items[0].title,
