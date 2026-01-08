bd88a38bbf50423fd2b04b06c5ab30610adf7777
Jordan Dichev <jordan.dichev@gmail.com>
2026-01-08 19:07:58 +0200

    Simplify style structure

--- webapp/src/index.tsx
+++ webapp/src/index.tsx
@@ -1,7 +1,7 @@
 import { createRoot } from "react-dom/client";
 
 import "bootstrap-icons/font/bootstrap-icons.css";
-import "./style/index.css";
+import "./index.css";
 
 import App from "./App";
 
